b61f4295862a75ed37f97a0770ee2680660b9e24
kagenKj <samusenko_nik@inbox.ru>
2024-09-24 21:25:36 +0300

    refactor: database connection in MainWindow

--- src/windows/main.py
+++ src/windows/main.py
@@ -1,5 +1,7 @@
+from PyQt6 import QtSql
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtGui import QAction
+import logging
 
 from src.windows.analytics import AnalyticsDialog
 from src.windows.about import AboutDialog
@@ -13,6 +15,10 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setup_actions()
+        self.db = self.connect_database()  
+
+        if self.db:
+            self.setup_table_models()  
 
     def setup_actions(self):
         analytic_action = QAction("Аналитика", self)
@@ -27,9 +33,31 @@ class MainWindow(QMainWindow):
         export_action.triggered.connect(self.open_export)
         self.ui.export_2.addAction(export_action)
 
+    def connect_database(self):
+        db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName("DB/DataBase.sqlite")
+
+        if not db.open():
+            logging.error("Не удалось подключиться к базе данных.")
+            return False
+        return db  
+
+    def setup_table_models(self):
+        self.table1 = self.create_table_model("vuz", self.ui.tableView)
+        self.table2 = self.create_table_model("nir_grant", self.ui.tableView_2)
+        self.table3 = self.create_table_model("nir_ntp", self.ui.tableView_3)
+        self.table4 = self.create_table_model("nir_templan", self.ui.tableView_4)
+
+    def create_table_model(self, table_name, table_view):
+        model = QtSql.QSqlTableModel()
+        model.setTable(table_name)
+        model.select()
+        table_view.setModel(model)
+        return model
+
     def open_analytics(self):
-        self.analitycs_window = AnalyticsDialog()
-        self.analitycs_window.show()
+        self.analytics_window = AnalyticsDialog()
+        self.analytics_window.show()
 
     def open_about(self):
         self.about_window = AboutDialog()
@@ -38,3 +66,5 @@ class MainWindow(QMainWindow):
     def open_export(self):
         self.export_window = ExportDialog()
         self.export_window.show()
+
+    
