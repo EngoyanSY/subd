01d34f1bd8013704300b1fb7b34963a6a2b8e107
Nero <doglermr9@gmail.com>
2024-11-02 13:38:30 +0300

    fix: black flake

--- schema.py
+++ schema.py
@@ -1,11 +1,11 @@
-from typing import Optional, ClassVar, List
+from typing import Optional, ClassVar
 
 from pydantic import BaseModel, Field, field_validator
-from core import Session
+
 
 class BaseSchema(BaseModel):
 
-    @field_validator("grnti_code", mode='before', check_fields=False)
+    @field_validator("grnti_code", mode="before", check_fields=False)
     def validate_grnti_code(cls, value):
         if isinstance(value, int):
             return str(value)
@@ -27,7 +27,7 @@ class Nir_Grant(BaseSchema):
     director_academic_degree: Optional[str] = Field(default=None)
     table_name: ClassVar[str] = "Gr_pr.xlsx"
 
-    
+
 class Nir_NTP(BaseSchema):
     UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
     ntp_code: int = Field()
@@ -56,7 +56,7 @@ class Nir_Templan(BaseSchema):
     director_position: str = Field()
     table_name: ClassVar[str] = "Tp_pr.xlsx"
 
-    @field_validator("nir_reg_number", mode='before')
+    @field_validator("nir_reg_number", mode="before")
     def validate_nir_reg_number(cls, value):
         if isinstance(value, int):
             return str(value)
@@ -85,10 +85,8 @@ class Nir_GRNTI(BaseSchema):
     rubrika: str = Field()
     table_name: ClassVar[str] = "grntirub.xlsx"
 
-    @field_validator("codrub", mode='before')
+    @field_validator("codrub", mode="before")
     def validate_grnti_code(cls, value):
         if isinstance(value, int):
             return f"{value:02}"
         return value
-
-
